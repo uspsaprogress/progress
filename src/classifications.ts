7e3a183cc48289efc9db14a2853b8248c5c580b6
Casper <largebats@protonmail.org>
2025-06-23 21:59:14 -0700

    Get classification history

--- src/classifications.ts
+++ src/classifications.ts
@@ -1,3 +1,6 @@
+import { sortBy } from "lodash/fp";
+import _ from "lodash";
+
 export interface ClassifierScore {
     date: Date,
     classifierNumber: string,
@@ -9,7 +12,7 @@ export interface ClassifierScore {
 
 export class RollingWindow {
     constructor(public readonly scores: ClassifierScore[]) {
-        this.canScore = scores.length >= 6;
+        this.canScore = scores.length >= 4;
     }
 
     public readonly canScore: boolean;
@@ -25,4 +28,77 @@ export class RollingWindow {
         newHistory.push(nextClassifier);
         return new RollingWindow(newHistory);
     }
+
+    classificationScore(): number | null {
+        if (this.canScore) {
+            const dropScoreCount = Math.min(2, this.scores.length - 4);
+            return _(this.scores)
+                .map(s => s.percent)
+                .sortBy()
+                .slice(dropScoreCount)
+                .mean();
+        }
+        return null;
+    }
+}
+
+/**
+ * Classifiers are sorted by date in ascending order.
+ * 
+ * If multiple classifiers are shot on the same day, they are sorted by percentage in ascending order.
+ */
+export function sortClassifiers(scores: ClassifierScore[]): ClassifierScore[] {
+    return sortBy<ClassifierScore>([
+        s => s.date.getTime(),
+        s => s.percent
+    ])(scores);
+}
+
+/**
+ * Indicates whether we should exclude the given score from consideration based on the given flag.
+ *
+ * We are intentionally being more permissive than USPSA here. We want to consider P- and U-flagged classifiers
+ * (pending or unpaid) so that we don't have to wait until Tuesday for stats to run to see how a recent score will
+ * affect our classification. I'm also allowing X flags (submitted while the membership was inactive) so that people
+ * can continue to track their progress even if they're not paying.
+ *
+ * These decisions are somewhat arbitrary. In the future we could consider letting the user pick which flags to
+ * exclude using checkboxes or similar.
+ */
+export function isInvalid(flag: string): boolean {
+    switch (flag) {
+        case "I": // Administrative exclusion
+        case "Q": // DQ
+        case "N": // "Did Not Fire"
+            return true;
+        default:
+            return false
+    }
+}
+
+export interface ClassificationSnapshot {
+    readonly date: Date;
+    readonly percentage: number;
+};
+
+export function getClassificationHistory(classifiers: ClassifierScore[]): ClassificationSnapshot[] {
+    let window = new RollingWindow([]);
+    return _(classifiers)
+        .sortBy([
+            s => s.date.getTime(),
+            s => s.percent
+        ]).reduce<ClassificationSnapshot[]>(
+            (hist, nextScore) => {
+                if (nextScore.flag !== undefined && !isInvalid(nextScore.flag)) {
+                    window = window.append(nextScore);
+                    if (window.canScore) {
+                        hist.push({
+                            date: nextScore.date,
+                            percentage: window.classificationScore()!
+                        });
+                    }
+                }
+                return hist;
+            }, []
+        )
 }
--- test/classification.test.ts
+++ test/classification.test.ts
@@ -4,19 +4,20 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 import { parseTextInput } from "../src/parsing";
-import { RollingWindow } from "../src/classifications";
+import { ClassifierScore, ClassificationSnapshot, getClassificationHistory, RollingWindow, sortClassifiers } from "../src/classifications";
 
 const classifierData = parseTextInput(readFileSync(join(__dirname, "data", "record.txt"), { encoding: "utf-8" }));
 
+function quickScore(date: string, classifierNumber: string, percent: number): ClassifierScore {
+    return {
+        date: new Date(date),
+        classifierNumber: classifierNumber,
+        percent: percent
+    };
+}
+
 describe("RollingWindow", () => {
     describe("append()", () => {
-        function quickScore(date: string, classifierNumber: string, percent: number): ClassifierScore {
-            return {
-                date: new Date(date),
-                classifierNumber: classifierNumber,
-                percent: percent
-            };
-        }
 
         const scores = [
             quickScore("01/01/24", "01-01", 55),
@@ -65,5 +66,116 @@ describe("RollingWindow", () => {
                 window.append(newScore).scores
             );
         });
+    });
+
+    describe("sortClassifiers()", () => {
+        it("should sort by date and percentage", () => {
+            const classifiers = [
+                quickScore("01/01/24", "01-01", 55),
+                quickScore("02/02/24", "02-01", 58),
+                quickScore("01/01/24", "01-05", 34),
+                quickScore("01/01/24", "08-09", 90),
+                quickScore("02/02/24", "07-05", 80)
+            ];
+
+            const expected = [
+                quickScore("01/01/24", "01-05", 34),
+                quickScore("01/01/24", "01-01", 55),
+                quickScore("01/01/24", "08-09", 90),
+                quickScore("02/02/24", "02-01", 58),
+                quickScore("02/02/24", "07-05", 80)
+            ]
+
+            assert.deepEqual(sortClassifiers(classifiers), expected);
+        });
+    });
+
+    describe("classificationScore()", () => {
+        it("should match USPSA's calculation for a known record", () => {
+            const score = sortClassifiers(classifierData)
+                .reduce<RollingWindow>((window, s) => window.append(s), new RollingWindow([]))
+                .classificationScore()?.toFixed(4);
+            assert.equal(score, 96.6455)
+        });
+    });
+
+    describe("getClassificationHistory()", () => {
+        const expected: ClassificationSnapshot[] = [
+            {
+                date: new Date("07/15/23"),
+                percentage: 76.80225
+            },
+            {
+                date: new Date("08/06/23"),
+                percentage: 82.60745
+            },
+            {
+                date: new Date("12/03/23"),
+                percentage: 85.32589999999999
+            },
+            {
+                date: new Date("01/07/24"),
+                percentage: 79.19494999999999
+            },
+            {
+                date: new Date("01/26/24"),
+                percentage: 82.15845999999999
+            },
+            {
+                date: new Date("03/03/24"),
+                percentage: 80.38911666666665
+            },
+            {
+                date: new Date("03/03/24"),
+                percentage: 82.87986666666666
+            },
+            {
+                date: new Date("03/03/24"),
+                percentage: 85.74546666666667
+            },
+            {
+                date: new Date("03/03/24"),
+                percentage: 88.46071666666666
+            },
+            {
+                date: new Date("03/03/24"),
+                percentage: 89.53298333333333 
+            },
+            {
+                date: new Date("03/03/24"),
+                percentage: 91.08616666666667
+            },
+            {
+                date: new Date("03/03/24"),
+                percentage: 92.42359999999998 
+            },
+            {
+                date: new Date("03/03/24"),
+                percentage: 93.42151666666666
+            },
+            {
+                date: new Date("06/02/24"),
+                percentage: 93.42151666666666
+            },
+            {
+                date: new Date("07/07/24"),
+                percentage: 94.8756
+            },
+            {
+                date: new Date("09/01/24"),
+                percentage: 94.8756
+            },
+            {
+                date: new Date("11/03/24"),
+                percentage: 96.10669999999999
+            },
+            {
+                date: new Date("06/01/25"),
+                percentage: 96.64551666666667
+            }
+        ];
+        // assert.deepEqual(getClassificationHistory(classifierData), expected);
+        const result = getClassificationHistory(classifierData)
+        expected.forEach((val, index) => assert.deepEqual(result[index], expected[index]));
     });
 });
